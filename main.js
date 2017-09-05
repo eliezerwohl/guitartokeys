21b9cc4df42f3bb82d0dd9f88934fc020eecc1d2
eliezerwohl <eliezerwohl@gmail.com>
2017-09-05 16:13:39 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,34 @@
+
+var notesArray = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"];
+var stringNotes = ["E", "A", "D", "G", "B", "E"]
+var reverseString = stringNotes.reverse()
+var neck= 12;
+
+for (var i = 0; i < reverseString.length; i++) {
+	console.log("this is the guitar string" + reverseString[i])
+
+	var location1 = notesArray.indexOf(reverseString[i])
+	// console.log(location1)
+ var counter = 0;
+	for (var j = location1; j < neck; j++) {
+		counter++;
+
+		console.log(notesArray[j])
+				if (j == notesArray.length - 1 ){
+			console.log("done with neck")
+			j = -1; 
+
+		}
+		if (counter == neck){
+			break;
+		}
+	}
+}
+// for (var i = 0; i < notesArray.length; i++) {
+// 	console.log(notesArray[i])
+// }
+
+// var guitar = document.getElementById('guitar')
+// theKid = document.createElement("button");
+// theKid.innerHTML = 'Are we there yet?';
+// guitar.appendChild(theKid)
